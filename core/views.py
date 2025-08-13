08b81e28d3530cd4103bc127d819fd06777ae839
alaminosman <00966597543717mail@gmail.com>
2025-08-13 08:40:54 +0000

    Added base template and updated other templates to use it

--- core/views.py
+++ core/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def home(request):
     posts = Post.objects.all()  # Fetches all Post objects from the database
     return render(request, 'core/home.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'core/post_detail.html', {'post': post})
 
 # Create your views here.
